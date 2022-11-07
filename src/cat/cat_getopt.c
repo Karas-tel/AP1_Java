8dd1d493d868ad09c04d0faa12436fb0331361a6
shelacor <k-i-a-t@yandex.ru>
2022-11-07 17:14:32 +0300

    develop_C3

--- src/cat/cat_getopt.c
+++ src/cat/cat_getopt.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <stdlib.h>
+
+void print_file(FILE *file, int number, int number_nonblank, int squeeze_blank,
+                int show_ends, int show_nonprinting, int show_tabs);
 
 int main(int argc, char *argv[]) {
 	int flag = 0;
-    int flags[6][2] = {
-        { (int)'b', 0 },
-        { (int)'e', 0 },
-        { (int)'n', 0 },
-        { (int)'s', 0 },
-        { (int)'t', 0 },
-        { (int)'v', 0 }
-    };
+    int number = 0;
+    int number_nonblank = 0;
+    int squeeze_blank = 0;
+    int show_ends = 0;
+    int show_nonprinting = 0;
+    int show_tabs = 0;
     
 	const char* short_options = "beEnstTv";
 
@@ -26,29 +28,124 @@ int main(int argc, char *argv[]) {
 
 	while ((flag = getopt_long(argc, argv, short_options, long_options, NULL)) != -1){
 		switch (flag) {
-		case 'b': flags[0][1] = 1; break;
-		case 'e': flags[1][1] = 1; flags[5][1] = 1; break;
-		case 'E': flags[1][1] = 1; break;
-		case 'n': flags[2][1] = 1; break;
-		case 's': flags[3][1] = 1; break;
-        case 't': flags[4][1] = 1; flags[5][1] = 1; break;
-        case 'T': flags[4][1] = 1; break;
-        case 'v': flags[5][1] = 1; break;
-        default : fprintf(stderr, "invalid option"); break; 
+		case 'b': 
+            number = 1;
+            number_nonblank = 1;
+            break;
+		case 'e':
+            show_ends = 1;
+            show_nonprinting = 1;
+            break;
+		case 'E':
+            show_ends = 1;
+            break;
+		case 'n':
+            number = 1;
+            break;
+		case 's': 
+            squeeze_blank = 1;
+            break;
+        case 't':
+            show_tabs = 1;
+            show_nonprinting = 1;
+            break;
+        case 'T':
+            show_tabs = 1;
+            break;
+        case 'v':
+            show_nonprinting = 1; 
+            break;
+        default : fprintf(stderr, "s21_cat: illegal option\nusage: ./s21_cat [-beEnstTv] [file] ...\n"); break; 
 		}
     }
 
     if (optind >= argc) {
-        fprintf(stderr, "no input file");
+        fprintf(stderr, "s21_cat: illegal option\nusage: ./s21_cat [-beEnstTv] [file] ...\n");
     }
 
     if (optind < argc) {
         do {
-            char *file = argv[optind];
-            // do something with file
-            printf("%s\t", file);
+            char *file_name = argv[optind];
+            FILE *file;
+            if ((file = fopen(file_name, "r")) == NULL) {
+                fprintf(stderr, "s21_cat: %s: No such file or directory", file_name);
+            } else {
+                print_file(file, number, number_nonblank, squeeze_blank,
+                show_ends, show_nonprinting, show_tabs);
+                fclose(file);
+            }
         } while ( ++optind < argc);
     }
 
     return 0;
+}
+
+void print_file(FILE *file, int number, int number_nonblank, int squeeze_blank,
+                int show_ends, int show_nonprinting, int show_tabs) {
+    int size_text = 256;
+    char* text = calloc(sizeof(char), size_text);
+    char c;
+    int iter = 0, number_string = 1;
+    while (fscanf(file, "%c", &c) == 1) {
+        text[iter] = c;
+        if (iter > size_text - 10) {
+            size_text *= 2;
+            text = (char *) realloc(text, size_text);
+        }
+        iter++;
+    }
+    
+    if (number_nonblank == 1) {
+        if (0 < iter && text[0] != '\n')
+            fprintf(stdout, "%6d\t", number_string++);
+    } else if (number == 1) {
+        if (0 < iter)
+            fprintf(stdout, "%6d\t", number_string++);
+    }
+
+    for (int i = 0; i < iter; ++i) {
+        c = text[i];
+        int flag_work_5 = 0;
+
+        if (squeeze_blank == 1) {
+            if (i < iter - 2 && c == '\n' && c == text[i+1] && text[i+1] == text[i+2] ) { continue; }
+        }
+
+        if (show_nonprinting == 1) {
+            if (((int)c >= 0 && (int)c < 32 && c != '\n' && (int)c != 9) || (int)c == 127) {
+                if ((int)c != 127) {
+                    c = (char)((int)c + 64);
+                } else c = '?';
+                flag_work_5 = 1;
+            }
+        }
+
+        if (show_ends == 1) {
+            if (c == '\n') {
+                fprintf(stdout, "$");
+                flag_work_5 = 0;
+            }
+        }
+
+        if (show_tabs == 1) {
+            if (c == (char)9) {
+                c = 'I'; 
+                flag_work_5 = 1;
+            }
+        }
+        
+        if (flag_work_5 == 1) {
+            fprintf(stdout, "^");
+        }
+        fprintf(stdout, "%c", c);
+
+        if (number_nonblank == 1) {
+            if (i < iter - 1 && c == '\n' && c != text[i+1])
+                fprintf(stdout, "%6d\t", number_string++);
+        } else if (number == 1) {
+            if (c == '\n' && i < iter - 1)
+                fprintf(stdout, "%6d\t", number_string++);
+        }
+    }
+    free(text);
 }
