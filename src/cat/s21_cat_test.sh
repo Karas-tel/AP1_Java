b35f79bbda06935ccd1614cf6dda61dad98df4e1
Shela Coralie <shelacor@at-g5.msk.21-school.ru>
2022-11-22 15:32:20 +0300

    develop_C3

---
+++ src/cat/s21_cat_test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+SUCCESS=0
+FAIL=0
+COUNTER=0
+DIFF=""
+STR="FLAGS FILES"
+
+declare -a file=(
+" cat_tests\/file_1 "
+" cat_tests\/file_2 "
+" cat_tests\/file_3 "
+" cat_tests\/file_4 "
+)
+
+declare -a flag=(
+" -b "
+" -e "
+" -n "
+" -s "
+" -t "
+" -be "
+" -en "
+" -ns "
+" -st "
+" -tb "
+# " --number-nonblank"
+# " -E "
+# " --number "
+# " --squeeze-blank "
+# " -T "
+)
+
+testing()
+{
+    str=$(echo $STR | sed "s/FLAGS/$flags/g;s/FILES/$files/g")
+    ./s21_cat $str > s21_cat_test.log
+    cat $str > cat_test.log
+    DIFF="$(diff s21_cat_test.log cat_test.log)"
+    (( COUNTER++ ))
+ 
+    if [ "$DIFF" == "" ]
+    then
+      (( SUCCESS++ ))
+      echo "$FAIL $SUCCESS $COUNTER success cat $str"
+    else
+      (( FAIL++ ))
+      echo "$FAIL $SUCCESS $COUNTER fail cat $str"
+    fi
+    rm s21_cat_test.log cat_test.log
+}
+
+getfiles()
+{
+    for files in "${file[@]}"
+    do
+        testing
+    done
+}
+
+for flags in "${flag[@]}"
+do
+    getfiles 
+done
+
+
+echo "FAIL: $FAIL"
+echo "SUCCESS: $SUCCESS"
+echo "ALL: $COUNTER"
